d7b783a63f7cf68d1842f0f5930675d483660f0e
whalewalker <mongodbms@gmail.com>
2021-08-10 06:37:10 +0100

    setup spring security config

--- src/main/java/com/library/library/service/BookServiceImpl.java
+++ src/main/java/com/library/library/service/BookServiceImpl.java
@@ -1,17 +1,24 @@
 package com.library.library.service;
 
+import com.cloudinary.utils.ObjectUtils;
 import com.library.library.domain.dto.BookDto;
 import com.library.library.domain.models.Book;
 import com.library.library.domain.repository.AuthorRepository;
 import com.library.library.domain.repository.BookRepository;
 import com.library.library.domain.repository.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class BookServiceImpl implements BookService{
     @Autowired
     BookRepository bookRepository;
@@ -21,6 +28,9 @@ public class BookServiceImpl implements BookService{
 
     @Autowired
     CategoryRepository categoryRepository;
+
+    @Autowired
+    CloudStorageService cloudStorageService;
 
 
     @Override
@@ -44,8 +54,32 @@ public class BookServiceImpl implements BookService{
         if (bookDto  == null) throw new NullPointerException("Book cannot be null");
         Book book = new Book();
 
+        if (bookDto.getCoverImage() != null && !bookDto.getCoverImage().isEmpty()){
+            try {
+                Map<?, ?> uploadResult = cloudStorageService.uploadImage(bookDto.getCoverImage(), ObjectUtils.asMap(
+                        "library/coverImages" + extractFileName(Objects.requireNonNull(bookDto.getCoverImage().getOriginalFilename()))
+                ));
 
-        return null;
+                book.setCoverImage(String.valueOf(uploadResult.get("url")));
+                log.info("Image url from cloudinary --> {}", uploadResult.get("url"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        book.setBookUrl("/home/whalewalker/Whalewalker/Personal/library/src/main/resources/static/Spring Boot Up and Running Building Cloud Native Java and Kotlin Applications by Mark Heckler [Mark Heckler] (z-lib.org).pdf");
+        book.setPublisher("Impact");
+
+       try {
+           return bookRepository.save(book);
+       }catch (DataIntegrityViolationException err){
+           log.info("Exception occurred --> {}", err.getMessage());
+           throw err;
+       }
+    }
+
+    private String extractFileName(String filename){
+        return filename.split("\\.")[0];
     }
 
     @Override
--- src/test/java/com/library/library/service/CloudStorageServiceImplTest.java
+++ src/test/java/com/library/library/service/CloudStorageServiceImplTest.java
@@ -1,19 +1,27 @@
 package com.library.library.service;
 
+import com.cloudinary.utils.ObjectUtils;
+import com.library.library.domain.dto.BookDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Slf4j
@@ -45,6 +53,33 @@ class CloudStorageServiceImplTest {
             log.info("Error occurred --> {}", e.getMessage());
         }
 
+    }
+
+    @Test
+    @DisplayName("Uplaod multipart file test")
+    void uploadImageTest() throws IOException {
+        BookDto bookDto = new BookDto();
+        bookDto.setTitle("Springboot");
+        bookDto.setDescription("Springboot for beginner");
+
+        Path path = Paths.get("/home/whalewalker/Whalewalker/Personal/library/src/main/resources/static/me.jpeg");
+        MultipartFile multipartFile = new MockMultipartFile("book_img", "book_img", "img/jpg", Files.readAllBytes(path));
+
+        log.info("Multipart Object is created --> {}", multipartFile);
+        assertThat(multipartFile).isNotNull();
+
+        bookDto.setCoverImage(multipartFile);
+
+        log.info("File name --> {}", bookDto.getCoverImage().getOriginalFilename());
+        cloudStorageServiceImpl.uploadImage(multipartFile, ObjectUtils.asMap(
+                "public_id", "library" + extractedFileName(Objects.requireNonNull(bookDto.getCoverImage().getOriginalFilename()))
+        ));
+
+        assertThat(bookDto.getCoverImage().getOriginalFilename()).isEqualTo("book_img");
+    }
+
+    private String extractedFileName(String filename) {
+        return filename.split("\\.")[0];
     }
 
 }
