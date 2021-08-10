7c2b6e3d469e76018e1b86151b446366afe9ed75
whalewalker <mongodbms@gmail.com>
2021-08-10 06:40:59 +0100

    configure web mvc configurer

---
+++ src/main/java/com/library/library/web/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.library.library.web.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        long MAX_AGE_SECS = 3600;
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCh", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
